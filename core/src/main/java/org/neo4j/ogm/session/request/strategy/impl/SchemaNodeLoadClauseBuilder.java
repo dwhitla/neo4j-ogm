c5f31a86b5025221619d19ce5a803b567612d09e
Michael J. Simons <michael.simons@neo4j.com>
2018-11-05 18:32:55 +0100

    Make the SchemaNodeLoadClauseBuilder handle depth=-1 queries.
    This uses the schema and traverses all relationships explicit, avoiding a match of the whole graph.

--- core/src/main/java/org/neo4j/ogm/session/request/strategy/impl/SchemaNodeLoadClauseBuilder.java
+++ core/src/main/java/org/neo4j/ogm/session/request/strategy/impl/SchemaNodeLoadClauseBuilder.java
@@ -13,7 +13,16 @@
 
 package org.neo4j.ogm.session.request.strategy.impl;
 
+import static java.util.stream.Collectors.*;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
 import org.neo4j.ogm.metadata.schema.Node;
+import org.neo4j.ogm.metadata.schema.Relationship;
 import org.neo4j.ogm.metadata.schema.Schema;
 import org.neo4j.ogm.session.request.strategy.LoadClauseBuilder;
 
@@ -21,6 +30,7 @@ import org.neo4j.ogm.session.request.strategy.LoadClauseBuilder;
  * Schema based load clause builder for nodes - starts from given node variable
  *
  * @author Frantisek Hartman
+ * @author Michael J. Simons
  */
 public class SchemaNodeLoadClauseBuilder extends AbstractSchemaLoadClauseBuilder implements LoadClauseBuilder {
 
@@ -29,8 +39,9 @@ public class SchemaNodeLoadClauseBuilder extends AbstractSchemaLoadClauseBuilder
     }
 
     public String build(String variable, String label, int depth) {
+
         if (depth < 0) {
-            throw new IllegalArgumentException("Only queries with depth >= 0 can be built, depth=" + depth);
+            return createExplicitMatchAndReturnClauses(label, variable, depth);
         }
 
         StringBuilder sb = new StringBuilder();
@@ -48,4 +59,78 @@ public class SchemaNodeLoadClauseBuilder extends AbstractSchemaLoadClauseBuilder
         return sb.toString();
     }
 
+    String createExplicitMatchAndReturnClauses(String label, String variable, int maxDepth) {
+
+        StringBuilder loadClause = new StringBuilder();
+        RelationshipIterator iterator = new RelationshipIterator(schema.findNode(label), variable, maxDepth);
+        while (iterator.hasNext()) {
+            loadClause.append(" ").append(iterator.next());
+        }
+
+        String returnClause = Stream.iterate(0, i -> i + 1).limit(iterator.getDepth())
+            .map(i -> "p" + i)
+            .collect(joining(",", " RETURN ", ""));
+        loadClause.append(returnClause);
+
+        return loadClause.toString();
+    }
+
+    static class RelationshipIterator implements Iterator<String> {
+
+        private final String variable;
+        private final int maxDepth;
+
+        private List<Relationship> traversableRelationships;
+        private final Set<Node> visitedSourceNode = new HashSet<>();
+        private int depth = 0;
+
+        RelationshipIterator(Node startNode, String variable, int maxDepth) {
+
+            this.traversableRelationships = extractRelationships(startNode).collect(toList());
+            this.variable = variable;
+            this.maxDepth = maxDepth < 0 ? Integer.MAX_VALUE : maxDepth;
+        }
+
+        private static Stream<Relationship> extractRelationships(Node node) {
+            return node.relationships().values().stream();
+        }
+
+        private static String toTypePattern(Relationship relationship) {
+            return ":`" + relationship.type() + "`";
+        }
+
+        /**
+         * @return The currently reached depth.
+         */
+        public int getDepth() {
+            return depth;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.depth <= this.maxDepth && !traversableRelationships.isEmpty();
+        }
+
+        @Override
+        public String next() {
+
+            String s = traversableRelationships.stream().map(RelationshipIterator::toTypePattern).sorted().collect(joining("|"));
+            String m = String.format("MATCH p%d=(%s%s)-[%s*0..]-(%2$s%d)", depth, variable,
+                depth == 0 ? "" : Integer.toString(depth), s, depth + 1);
+
+            // Store all newly visited nodes
+            this.visitedSourceNode
+                .addAll(this.traversableRelationships.stream().map(Relationship::start).collect(toList()));
+
+            // Recompute the next, traversable relationships
+            this.traversableRelationships = this.traversableRelationships.stream()
+                .map(r -> r.other(r.start()))
+                .filter(n -> !visitedSourceNode.contains(n))
+                .flatMap(RelationshipIterator::extractRelationships)
+                .collect(toList());
+
+            depth += 1;
+            return m;
+        }
+    }
 }
